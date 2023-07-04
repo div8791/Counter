f03e4e90aa7e11492139da6bf842fd8dce654a68
Divyarth Singh <div8791@gmail.com>
2023-07-05 00:06:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const countVal = document.querySelector('#counter');
+const increment = ()=>{
+    let value = parseInt(countVal.innerText);
+    value = value + 1;
+    countVal.innerText = value;
+};
+
+const decrement = ()=>{
+    let value = parseInt(countVal.innerText);
+    value = value - 1;
+    countVal.innerText = value;
+};
